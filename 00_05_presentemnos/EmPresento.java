ee25f2bdddf0c2690bc43af26c26340039e77593
Kil <kilianfernandez84@gmail.com>
2025-10-01 09:02:49 +0200

    Ejercicio 00_05 completado

---
+++ 00_05_presentemnos/EmPresento.java
@@ -0,0 +1,6 @@
+/* Programa que imprimeix la presentació */
+public class EmPresento {
+    public static void main(String[] args) {
+        System.out.println("Soy Kil, el mejor delegado del TIC :P");
+    }
+}
