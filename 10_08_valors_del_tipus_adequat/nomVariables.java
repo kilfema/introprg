5b15bfe4275ad01dba38c218c82563b7a15fe508
Kil <kilianfernandez84@gmail.com>
2025-10-03 12:32:45 +0200

    Ejercicio 10_08 completado

---
+++ 10_08_valors_del_tipus_adequat/nomVariables.java
@@ -0,0 +1,24 @@
+/*
+Declaració i incialització de diferents tipus de variables
+*/
+
+public class nomVariables {
+    public static void main(String[] args) {
+        
+        
+        //S'introdueixen variables
+        double tallaSabates = 42.5;
+        
+        String dataAniversari = "8 d'abril de 2003";
+        
+        String nom = "Kil";
+        
+        String colorFonsEscriptori = "Negre";
+        
+        boolean agradaLlimona = true;
+        
+        
+        //Mostra resultat
+        System.out.println("La meva talla de sabates és " + tallaSabates + ", el meu aniversari és el " + dataAniversari + " i em dic " + nom + ". A més, el color del meu fons de pantalla és " + colorFonsEscriptori + " i si em preguntessis si m'agrada la llimona, la resposta booleana és " + agradaLlimona);
+    }
+}
