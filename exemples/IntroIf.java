96e8eac38bba7d46fc46fb658eaf177473401b82
Kil <kilianfernandez84@gmail.com>
2025-10-06 12:20:03 +0200

    Exemple

---
+++ exemples/IntroIf.java
@@ -0,0 +1,19 @@
+/*
+*En aquest exemple practicarem if / else
+*/
+
+public class IntroIf {
+    public static void main(String[] args){
+        boolean faSol = true;
+        
+        System.out.println("Avui fa sol: " + faSol);
+        //si fa sol -> "Qué bonica sa quedao la tarde"
+        //no fa sol -> "Qué feo, me quedo en casa con el Netflix"
+        
+        if (faSol == true) {
+            System.out.println("Qué bonica sa quedao la tarde");
+        }
+    
+    
+    }
+}
