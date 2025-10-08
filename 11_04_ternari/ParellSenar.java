e9943d89730a4f398b03e03f83679d8dd665970c
Kil <kilianfernandez84@gmail.com>
2025-10-08 09:55:59 +0200

    Canvi nom

--- 11_04_ternari/ParellSenar.java
+++ 11_04_ternari/ParellSenar.java
@@ -4,7 +4,7 @@
  * és parell o senar.
  * Si no s'especifica cap número, el resultat és indeterminat.
  */
-public class Ternari {
+public class ParellSenar {
     public static void main(String[] args) {
         int numero = Integer.parseInt(args[0]);
         
