72b1adda9c08d3db29925539cf4bbd5820354e09
Kil <kilianfernandez84@gmail.com>
2025-10-08 10:54:03 +0200

    Ejercicio 11_06 completado

---
+++ 11_06_autocaravana/Autocaravana.java
@@ -0,0 +1,25 @@
+/*
+ * Kilian Fernández - 8/10/2025
+ * Aquest programa categoritza les caravanes bassant-se en el seu preu. Per fer-ho, utilitza condicionals
+ */
+ 
+public class Autocaravana {
+    public static void main(String[] args) {
+        
+        //Demana valor
+        int preu = Integer.parseInt(Entrada.readLine());
+        
+        //Si val menys de 50.000
+        if (preu <50000) {
+            System.out.println("Econòmica");
+            
+        //si val entre 50.000 i 175.000
+        } else if ((preu >=50000) && (preu <=175000)) {
+            System.out.println("General");
+            
+        //si val més de 175.000
+        } else {
+            System.out.println("Luxe");
+        }
+    }
+}
