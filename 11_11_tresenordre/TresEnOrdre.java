d366bb7bcf0ba376e78bb47ae3b80875ebb023d0
Kil <kilianfernandez84@gmail.com>
2025-10-09 10:22:49 +0200

    Ejercicio 11_11 completado

---
+++ 11_11_tresenordre/TresEnOrdre.java
@@ -0,0 +1,58 @@
+/* 
+ * Kilian Fernández - 9/10/2025
+ * Programa que pide tres números y los devuelve ordenados de menor a mayor
+ */
+ 
+public class TresEnOrdre {
+    public static void main(String[] args) {
+      
+      //demana primer num
+      System.out.println("Primer?");
+      int primer = Integer.parseInt(Entrada.readLine());
+      
+      //demana segon num
+      System.out.println("Segon?");
+      int segon = Integer.parseInt(Entrada.readLine());
+      
+      //demana tercer num
+      System.out.println("Tercer?");
+      int tercer = Integer.parseInt(Entrada.readLine());
+      
+      int mesGran = 0;
+      
+      int enMig = 0;
+      
+      int mesPetit = 0;
+      
+      //compara primer
+      if (primer > segon && primer > tercer) {
+        mesGran = primer;
+      } else if (primer > segon ^ primer > tercer) {
+        enMig = primer;
+      } else {
+        mesPetit = primer;
+      }
+      
+      //compara segon
+      if (segon > primer && segon > tercer) {
+        mesGran = segon;
+      } else if (segon > primer ^ segon > tercer) {
+        enMig = segon;
+      } else {
+        mesPetit = segon;
+      }
+      
+      //compara tercer
+       if (tercer > segon && tercer > primer) {
+        mesGran = tercer;
+      } else if (tercer > segon ^ tercer > primer) {
+        enMig = tercer;
+      } else {
+        mesPetit = tercer;
+      }
+      
+      
+      System.out.println(mesPetit + ", " + enMig + " i " + mesGran);
+      
+    }
+}
