0d284d61d6c1c2961877c6830608727a17171958
Kil <kilianfernandez84@gmail.com>
2025-10-15 10:31:07 +0200

    Ejercicio 11_15 completado

---
+++ 11_15_nombre_de_fills/NombreFills.java
@@ -0,0 +1,32 @@
+/*
+ * Aquest programa demana el nombre de fills que tens i et retorna un missatge
+ */
+ 
+public class NombreFills {
+    public static void main(String[] args) {
+    
+        //demana nombre de fills
+    
+        System.out.println("Quants fills tens?");
+        int nombre = Integer.parseInt(Entrada.readLine());
+
+        //condicionals
+        if (nombre < 0) {
+            System.out.println("No pots tenir menys de 0 fills!");
+            
+        } else if (nombre == 0) {
+            System.out.println("Tot el que t'has estalviat en bolquers!");
+            
+        } else if (nombre == 1) {
+            System.out.println("Compte de no mimar-lo massa!");
+            
+        } else if (nombre > 1 && nombre < 5) {
+            System.out.println("No t'avorreixes a casa, eh?");
+            
+        } else {
+            System.out.println("Tu sí fas país!");
+        }
+       
+        
+    }
+}
