b38be901d543f7a253f6ff480c91c60296ef35a3
Kil <kilianfernandez84@gmail.com>
2025-10-16 19:27:20 +0200

    Iniciado

---
+++ 11_16_ascensor/Ascensor.java
@@ -0,0 +1,23 @@
+/*
+ * Aquest programa simula un ascensor virtual. Demana la planta actual i quants pisos es volen pujar i retorna el resultat.
+ */
+public class Ascensor {
+    public static void main(String[] args) {
+    
+        //Demana valors
+        System.out.println("pis?");
+        String pis = Entrada.readLine();
+        
+        System.out.println("botó?");
+        String boto = Entrada.readLine();
+        
+        if (pis == "planta baixa" && boto == "pujar un") {
+            System.out.println("primer pis");
+        } else if (pis == "planta baixa" && boto == "pujar dos") {
+            System.out.println("segon pis");
+        } else {
+            System.out.println("Error");
+        }
+        
+    }
+}
