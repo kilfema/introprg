c89d3e6b7df6335f522b39c81d381e7aac317d08
Kil <kilianfernandez84@gmail.com>
2025-10-31 18:19:49 +0100

    Completado

---
+++ 12_03_informecaracter/InformeCaracter.java
@@ -0,0 +1,61 @@
+/*
+ *Kilian Fernández - 28/10/2025
+ * Aquest programa simula un informe on extreu diferents dades d'una cadena de text
+ */
+ 
+public class InformeCaracter {
+    public static void main(String[] args) {
+        
+        //Demana valors
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        
+        System.out.println("Posicio?");
+        int posicio = Integer.parseInt(Entrada.readLine());
+        
+        //comproba si text està vuit
+        if ((text.isEmpty() || text.isBlank())) {
+            System.out.println("Error");
+        
+        //comprova si la posicio està fora de rang   
+        } else if (posicio > text.length()) {
+            System.out.println("Fora de rang");
+         
+         //mostra resultats   
+        } else {
+        
+            char caracter = text.charAt(posicio);
+                  
+            //getName
+            System.out.println("Character.getName(\'" + caracter + "\') : " + Character.getName(caracter));
+            
+            //isDigit
+            System.out.println("Character.isDigit(\'" + caracter + "\') : " + Character.isDigit(caracter));
+            
+            //isJavaIdentifierStart
+            System.out.println("Character.isJavaIdentifierStart(\'" + caracter + "\') : " + Character.isJavaIdentifierStart(caracter));
+            
+            //isJavaIdentifierPartisDigit
+            System.out.println("Character.isJavaIdentifierPart(\'" + caracter + "\') : " + Character.isJavaIdentifierPart(caracter));
+            
+            //isLetter
+            System.out.println("Character.isLetter(\'" + caracter + "\') : " + Character.isLetter(caracter));
+            
+            //isLowerCase
+            System.out.println("Character.isLowerCase(\'" + caracter + "\') : " + Character.isLowerCase(caracter));
+            
+            //isUpperCase
+            System.out.println("Character.isUpperCase(\'" + caracter + "\') : " + Character.isUpperCase(caracter));
+            
+            //isWhitespace
+            System.out.println("Character.isWhitespace(\'" + caracter + "\') : " + Character.isWhitespace(caracter));
+            
+            //toLowerCase
+            System.out.println("Character.toLowerCase(\'" + caracter + "\') : " + Character.toLowerCase(caracter));
+            
+            //toUpperCase
+            System.out.println("Character.toUpperCase(\'" + caracter + "\') : " + Character.toUpperCase(caracter));
+        }
+        
+    }
+}
