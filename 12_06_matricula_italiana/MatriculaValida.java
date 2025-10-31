92c73ea01a804c586645553700c28ff4f571bc0f
Kil <kilianfernandez84@gmail.com>
2025-10-31 12:20:22 +0100

    Ok

--- 12_06_matricula_italiana/MatriculaValida.java
+++ 12_06_matricula_italiana/MatriculaValida.java
@@ -12,30 +12,39 @@ public class MatriculaValida {
         
         matricula = matricula.toUpperCase(); //convierte en mayuscula
         
-         //confirma si les posicions dels números ho son
-         boolean sonNumero = Character.isDigit(matricula.charAt(2)) && Character.isDigit(matricula.charAt(2)) && Character.isDigit(matricula.charAt(2));
+        //confirma si té llargada minima
+        boolean teLlargadaMinima = (matricula.length() == 6);
         
+        boolean esCorrecte = false;
+        
+        
+        if (teLlargadaMinima) {
+        //confirma si les posicions dels números ho son
+        boolean sonNumero = Character.isDigit(matricula.charAt(2)) && Character.isDigit(matricula.charAt(2)) && Character.isDigit(matricula.charAt(2));
+            
         //confirma si les posicions de les lletres ho son
         boolean sonLletra = Character.isLetter(matricula.charAt(0)) && Character.isLetter(matricula.charAt(1)) && Character.isLetter(matricula.charAt(5)) && Character.isLetter(matricula.charAt(6));
-        
+            
         //confirma si les lletres son caracters no permesos
-        
-        boolean esExcepcio = false;
-        
-        if ( 
+        boolean esExcepcio = ( 
             (matricula.charAt(0) == 'I' || matricula.charAt(0) == 'O' || matricula.charAt(0) == 'Q' || matricula.charAt(0) == 'U') || 
             (matricula.charAt(1) == 'I' || matricula.charAt(1) == 'O' || matricula.charAt(1) == 'Q' || matricula.charAt(1) == 'U') || 
             (matricula.charAt(5) == 'I' || matricula.charAt(5) == 'O' || matricula.charAt(5) == 'Q' || matricula.charAt(5) == 'U') || 
             (matricula.charAt(6) == 'I' || matricula.charAt(6) == 'O' || matricula.charAt(6) == 'Q' || matricula.charAt(6) == 'U')
-            ) {
-               esExcepcio = true; 
+            );
+
+            if (teLlargadaMinima && sonLletra && sonNumero && !(esExcepcio)) {
+            esCorrecte = true;
             }
-
-         if (sonLletra && sonNumero && !(esExcepcio)) {
+       }
+       
+       //envia missatge
+       if (esCorrecte) {
             System.out.println("És una matrícula italiana vàlida");
-         } else {
+            
+       } else {
             System.out.println("No és una matrícula italiana vàlida");
-         }
-
-    }
+       }
+    
+   }
 }
