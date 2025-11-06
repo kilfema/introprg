fa4753b6faeb7ddd4698d92d0d40e318dea13bfd
Kil <kilianfernandez84@gmail.com>
2025-11-06 22:15:39 +0100

    Ok

--- 13_10_estadistiques/Estadistiques.java
+++ 13_10_estadistiques/Estadistiques.java
@@ -7,7 +7,7 @@
 public class Estadistiques {
     public static void main(String[] args) {
     
-    System.out.println("Introdueix un valor:");
+    System.out.println("Introdueix un valor");
     int valorActual = Integer.parseInt(Entrada.readLine());
     
     int valorMaxim = valorActual;
