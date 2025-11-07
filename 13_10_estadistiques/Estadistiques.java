c8346d7151669e945322f837506e65664ca4c75f
prgtest <kilianfernandez84@gmail.com>
2025-11-07 11:51:44 +0100

    Ok ∾ prgtest autocommit 13_10 #4

--- 13_10_estadistiques/Estadistiques.java
+++ 13_10_estadistiques/Estadistiques.java
@@ -34,19 +34,17 @@ public class Estadistiques {
             valorActual = Integer.parseInt(Entrada.readLine());       
         }
         
-        float mitjana = 0;
+        double mitjana = 0;
   
         //si la suma de valores és mayor que 0 -- evita dividir por 0
         if (sumaValors > 0) {
             
-            mitjana = sumaValors / quantitatValors;  //calcula media 
+            mitjana = (double)sumaValors / quantitatValors;  //calcula media 
             
-            if(sumaValors % quantitatValors == 0){
-            System.out.println(String.format("El mínim és: %d\nLa mitjana és: %.1f\nEl màxim és: %d", valorMinim, mitjana, valorMaxim)); 
-            }else{
-                System.out.println(String.format("El mínim és: %d\nLa mitjana és: %f\nEl màxim és: %d", valorMinim, mitjana, valorMaxim)); 
+           
+            System.out.println(String.format("El mínim és: %d\nLa mitjana és: %f\nEl màxim és: %d", valorMinim, mitjana, valorMaxim)); 
             
-            }
+            
         } else if (valorActual < 0) {
             System.out.println("Cap valor vàlid introduït");
         }
