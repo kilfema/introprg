80a10be5ca0c9dbf05f4b173facfedaf1067b4a6
Kil <kilianfernandez84@gmail.com>
2025-11-07 12:29:27 +0100

    Completat

---
+++ 13_11_dowhile/SequenciaCreixent.java
@@ -0,0 +1,29 @@
+/*
+* Kilian Fernández - 6/11/2025
+* Programa que demana valors indefinidament
+*
+*/
+public class SequenciaCreixent {
+    public static void main(String[] args) {
+    
+    System.out.println("Introdueix un valor");
+    int valorActual = Integer.parseInt(Entrada.readLine());
+    
+    int valorMaxim = 0;
+    
+    int count = 0;
+    
+        do {
+            
+            count ++;
+            valorMaxim = valorActual;
+            
+            System.out.println("Introdueix un valor");
+            valorActual = Integer.parseInt(Entrada.readLine());
+               
+        } while (valorActual > valorMaxim);
+    
+    System.out.println("La longitud de la secüència creixent: " + count);
+    
+    }
+}
