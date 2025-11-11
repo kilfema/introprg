fd3d3c1d85583a2c18244bde47921a6dc85c27eb
Kil <kilianfernandez84@gmail.com>
2025-11-11 13:57:25 +0100

    COmpletado

--- 13_13_endevina_cancel/EndevinaNombre.java
+++ 13_13_endevina_cancel/EndevinaNombre.java
@@ -3,7 +3,7 @@
 * Programa que demana valors indefinidament
 *
 */
-public class EndevinaCancel {
+public class EndevinaNombre {
     public static void main(String[] args) {
     
     //valor pensat
