d7fdfccc1c218da661d6cb3e11737c6aa0b1a437
prgtest <kilianfernandez84@gmail.com>
2025-11-11 19:29:40 +0100

    Completado ∾ prgtest autocommit 13_14 #2

--- 13_14_trobaproxim/TrobaMesProxim.java
+++ 13_14_trobaproxim/TrobaMesProxim.java
@@ -19,32 +19,32 @@ public class TrobaMesProxim {
         int valorDonat = Integer.parseInt(Entrada.readLine());
         
         //mentre primer valor no sigui positiu o 0, error i segueix demanant un valor
-        while (valorDonat < 0) {
+        if (valorDonat < 0) {
             System.out.println("No s'ha introduït cap valor positiu\nIntrodueix un valor");
             valorDonat = Integer.parseInt(Entrada.readLine());
-        }
+        } else {
         
-        int distanciaAncora = Math.abs(ancora - valorDonat); //proximitat entre ancora i valor donat
-        int valorMesProxim = valorDonat; // quin és el valor més próxim, s'utilitzará per mostras el valor final
-        int distanciaMesProxim = distanciaAncora; //quina es la distancia més propera dels números donats, s'utilitza per comparar amb els seguents números.
-       
-        while (valorDonat >= 0) {
+            int distanciaAncora = Math.abs(ancora - valorDonat); //proximitat entre ancora i valor donat
+            int valorMesProxim = valorDonat; // quin és el valor més próxim, s'utilitzará per mostras el valor final
+            int distanciaMesProxim = distanciaAncora; //quina es la distancia més propera dels números donats, s'utilitza per comparar amb els seguents números.
+           
+            while (valorDonat >= 0) {
 
-            if (distanciaAncora < distanciaMesProxim) {
-                valorMesProxim = valorDonat;
-                distanciaMesProxim = distanciaAncora;
-            
-            } else if (distanciaAncora == distanciaMesProxim && valorDonat < valorMesProxim) {
-                valorMesProxim = valorDonat;
+                if (distanciaAncora < distanciaMesProxim) {
+                    valorMesProxim = valorDonat;
+                    distanciaMesProxim = distanciaAncora;
+                
+                } else if (distanciaAncora == distanciaMesProxim && valorDonat < valorMesProxim) {
+                    valorMesProxim = valorDonat;
+                }
+                
+            System.out.println("Introdueix un valor");
+            valorDonat = Integer.parseInt(Entrada.readLine());
+            distanciaAncora = Math.abs(ancora - valorDonat);
             }
             
-        System.out.println("Introdueix un valor");
-        valorDonat = Integer.parseInt(Entrada.readLine());
-        distanciaAncora = Math.abs(ancora - valorDonat);
+            System.out.println("El valor introduït més pròxim a " + ancora + " és " + valorMesProxim);
         }
-        
-        System.out.println("El valor introduït més pròxim a " + ancora + " és " + valorMesProxim);
-        
     }
     
     
