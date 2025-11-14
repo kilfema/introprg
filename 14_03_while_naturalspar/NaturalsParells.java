4b54e1afe65749c8898fe63f49228732275d05b1
Kil <kilianfernandez84@gmail.com>
2025-11-15 00:09:25 +0100

    Completat

---
+++ 14_03_while_naturalspar/NaturalsParells.java
@@ -0,0 +1,29 @@
+/*
+* Kilian Fernández - 14/11/2025
+* Programa que rep un numero enter positiu i compta els números parells fins arribar al número donat
+*
+*/
+public class NaturalsParells {
+    public static void main(String[] args) {
+    
+    //valor inicial 
+    int i = Integer.parseInt(args[0]);
+    
+    if (i<2) { //si és menor que 1
+        System.out.println("Cap valor creixent entre 1 i " + i);  
+    
+    } else {
+        
+        int comptador = 2;
+
+        while (comptador < i) { //mentres el comptador no arribi a l'index, mostra en pantalla i suma 2
+            System.out.println(comptador);
+            comptador+=2;
+        }
+    //imprimeix valor final
+    System.out.println(i);
+    
+    }
+    
+    }
+}
