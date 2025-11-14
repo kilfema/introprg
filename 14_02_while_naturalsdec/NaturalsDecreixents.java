5be94fb5b90097628ec4a134aa23a7d41fb017a8
Kil <kilianfernandez84@gmail.com>
2025-11-14 23:59:00 +0100

    Completado

---
+++ 14_02_while_naturalsdec/NaturalsDecreixents.java
@@ -0,0 +1,29 @@
+/*
+* Kilian Fernández - 14/11/2025
+* Programa que rep un numero enter positiu i compta des del número rebut fins arribar a 1
+*
+*/
+public class NaturalsDecreixents {
+    public static void main(String[] args) {
+    
+    //valor inicial 
+    int i = Integer.parseInt(args[0]);
+    
+    if (i<1) { //si és menor que 1
+        System.out.println("Cap valor decreixent entre 1 i " + i);  
+    
+    } else {
+        
+        int comptador = i;
+
+        while (comptador != 1) { //mentres el comptador no arribi a 1
+            System.out.println(comptador);
+            --comptador;
+        }
+    //imprimeix valor final
+    System.out.println(1);
+    
+    }
+    
+    }
+}
