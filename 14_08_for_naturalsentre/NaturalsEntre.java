0a94d4fb7694e37030a291e3a7b92be98adc4f44
Kil <kilianfernandez84@gmail.com>
2025-11-16 20:04:06 +0100

    Completat

---
+++ 14_08_for_naturalsentre/NaturalsEntre.java
@@ -0,0 +1,41 @@
+/*
+* Kilian Fernández - 14/11/2025
+*
+* Programa que demana tres enters no negatius (inici, final i salt),
+* en valida l’ordre i els valors, i si són correctes mostra tots els
+* nombres entre inici i final avançant segons el salt. Si algun valor
+* no és vàlid, mostra "Valor no vàlid" i acaba.
+*
+*/
+public class NaturalsEntre {
+    public static void main(String[] args) {
+    
+    System.out.println("Valor inicial?");
+    int valorInicial = Integer.parseInt(Entrada.readLine());
+    String error = "Valor no vàlid"; //missatge d'error
+    
+        if (valorInicial >= 0) { //comprova que el primer valor no sigui negatiu
+            
+            //demana valor final
+            System.out.println("Valor final?");
+            int valorFinal = Integer.parseInt(Entrada.readLine());
+            
+            if (valorFinal >= valorInicial) {
+            
+            //demana salt
+            System.out.println("Salt?");
+            int salt = Integer.parseInt(Entrada.readLine());
+            
+                if (salt > 0) {
+                    
+                for (int comptador = valorInicial; comptador < valorFinal; comptador= comptador + salt) {
+                    System.out.println(comptador);
+                }   
+
+                } else {System.out.println(error);}
+            
+            } else {System.out.println(error);}
+            
+        } else {System.out.println(error);} 
+    }
+}
