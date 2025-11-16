7946e78d5cfcc293c2cb2ff0579394c0b4f24904
Kil <kilianfernandez84@gmail.com>
2025-11-16 23:13:50 +0100

    Completado

---
+++ 14_10_mostrainterval/MostraInterval.java
@@ -0,0 +1,31 @@
+/*
+* Kilian Fernández - 14/11/2025
+*
+*Programa que demana dos enters i suma tots els valors entre ells (incloent-los), mostrant cada pas del càlcul. *Comença sempre pel nombre més petit i acumula la suma fins al més gran.
+*
+*/
+
+public class MostraInterval {
+    public static void main(String[] args) {
+    
+    System.out.println("primer?");
+    int primer = Integer.parseInt(Entrada.readLine());
+    
+    System.out.println("segon?");
+    int segon = Integer.parseInt(Entrada.readLine());
+    
+    if (primer < segon) {
+        
+        for (int i=primer; i <= segon ; i++) {
+            System.out.println(i);
+        }
+        
+    } else {
+    
+        for (int i=primer; i >= segon; i--) {
+            System.out.println(i);
+        }
+    }
+        
+    }
+}
