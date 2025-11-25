378a18db97d600f7a08d079a9702d960f47db31d
Kil <kilianfernandez84@gmail.com>
2025-11-25 01:34:28 +0100

    Ok

---
+++ InformeString.java
@@ -0,0 +1,21 @@
+/*
+ *Kilian Fernández - 28/10/2025
+ * Aquest programa simula un informe on extreu diferents dades d'una cadena de text
+ */
+public class InformeString {
+    public static void main(String[] args) {
+        
+    
+    System.out.println("Valor final?");
+    int v = Integer.parseInt(Entrada.readLine());
+    
+    for (int fila = 1; fila <= v; fila++) {
+            
+        for (int columna = 1; columna <= v; columna++) {
+        System.out.print(" " + columna);
+        }
+        
+    System.out.println();                   
+    }       
+    }
+}
