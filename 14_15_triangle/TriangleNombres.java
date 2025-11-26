b1f2a9506852d715fea71cca9d1be1977cb263a6
Kil <kilianfernandez84@gmail.com>
2025-11-26 11:00:40 +0100

    Completat

--- 14_15_triangle/TriangleNombres.java
+++ 14_15_triangle/TriangleNombres.java
@@ -1,25 +1,26 @@
 /*
  *Kilian Fernández - 28/10/2025
  * Aquest programa demana un enter entre 1 i 9 i imprimeix un triangle numèric creixent.
-*Per a cada línia, mostra els nombres des del valor de la línia fins a 1.
-*Si l’entrada no és vàlida, mostra el missatge “Valor inadequat”.
+ * Per a cada línia, mostra els nombres des del valor de la línia fins a 1.
+ * Si l’entrada no és vàlida, mostra el missatge “Valor inadequat”.
  */
 public class TriangleNombres {
     public static void main(String[] args) {
         
     
-    System.out.println("Valor final?");
+    System.out.println("Nombre?");
     int v = Integer.parseInt(Entrada.readLine());
         
         if (v < 1 || v > 9) {
         System.out.println("Valor inadequat");
         
         } else {
-        for (int fila = 1; fila <= v; fila++) {   
-            for (int columna = 1; columna <= v; columna++) {
-            System.out.print(" " + columna);
-            }  
-        System.out.println();                   
+        
+        for (int i = 1; i <=v; i++) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+        System.out.println("");
         }
         }
            
---
+++ 14_16_triangleinvertit/TriangleInvertit.java
@@ -0,0 +1,33 @@
+/*
+ *Kilian Fernández - 28/10/2025
+ * Aquest programa demana un enter positiu major que 0 i imprimeix un triangle numèric decreixent, des del nombre introduït fins a 1.
+ * A cada línia mostra els nombres de l’1 fins al valor de la línia.
+ * Si l’entrada no és vàlida, indica “Valor inadequat”.
+ */
+public class TriangleInvertit {
+    public static void main(String[] args) {
+        
+    
+    System.out.println("Nombre?");
+    int v = Integer.parseInt(Entrada.readLine());
+        
+        if (v < 1) {
+        System.out.println("Valor inadequat");
+        
+        } else {
+        
+        for (int i = 1; i <= v; i++) {
+            
+            for (int j = 1; j <= v - i + 1 ; j++) {
+                System.out.print(j);
+                if (!(j == v- i + 1)) System.out.print(", "); 
+                //si no correspon a la última fila, afegeix coma
+            }
+            
+        System.out.println("");
+        }
+        
+        }
+           
+    }
+}
