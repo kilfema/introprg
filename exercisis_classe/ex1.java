32d6902c7d30922eeeeca76ad6d5e3b4671e8b1a
Kil <kilianfernandez84@gmail.com>
2025-12-10 18:36:16 +0100

    Exercicis de classe

--- exercisis_classe/ex1.java
+++ exercisis_classe/ex1.java
@@ -4,17 +4,35 @@ public class ex1 {
     System.out.println("Introdueix una frase:");
     String frase = Entrada.readLine(); 
     
-    System.out.println("---------\n" + frase);
+    String fraseMin = frase.toLowerCase();
     
-    for (int pos = 0; pos < frase.length(); pos++) {
-        System.out.print(pos);
+    boolean teVocal = false;
+    
+    char caracter = 0;
+    
+    for (int i = 0; i < fraseMin.length(); i++) {
         
-        if (pos != frase.length() - 1) {
-            System.out.print(", ");
+        caracter = frase.charAt(i);
+        
+        if (caracter == 'a' || 
+            caracter == 'e' || 
+            caracter == 'i' || 
+            caracter == 'o' || 
+            caracter == 'u') {
+
+            teVocal = true;
         }
+    
     }
     
-    System.out.println("");
+    System.out.print("\n La frase " + frase);
+    
+    if (teVocal) {
+        System.out.print(" conté vocal.");
+    } else {
+        System.out.print(" no conté vocal.");
+    }
+
     
     }
 }
