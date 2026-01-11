e5e450a4f0dd3b7a34b90be3e9c2b413c944cf88
Kil <kilianfernandez84@gmail.com>
2026-01-11 15:55:51 +0100

    Completado

---
+++ 15_01_quadrats/Quadrats.java
@@ -0,0 +1,25 @@
+/*
+ *Kilian Fernández - 9/1/2026
+ * Programa que demana un número enter. Dibuixa tants quadrats com s'hagi indicat.
+ */
+ 
+public class Quadrats {
+    public static void main(String[] args) {
+    
+    System.out.println("Quants?");
+    int quadrats = Integer.parseInt(Entrada.readLine());
+    
+    if (quadrats < 1) return;
+    
+    for (int i = 0; i < quadrats; i++) {
+        
+        for (int j = 0; j < 5; j++) {
+            for (int k = 0; k < 5; k++) {
+                System.out.print("X");
+            }
+            System.out.println("");
+        }
+    System.out.println("");
+    }   
+    }
+}
