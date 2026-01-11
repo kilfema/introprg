6fbff43d42003acf25fb701d035eb2d388cdf921
prgtest <kilianfernandez84@gmail.com>
2026-01-11 15:56:38 +0100

    Completado ∾ prgtest autocommit 15_01 #1

--- 15_01_quadrats/Quadrats.java
+++ 15_01_quadrats/Quadrats.java
@@ -15,7 +15,7 @@ public class Quadrats {
         
         for (int j = 0; j < 5; j++) {
             for (int k = 0; k < 5; k++) {
-                System.out.print("X");
+                System.out.print(" X");
             }
             System.out.println("");
         }
