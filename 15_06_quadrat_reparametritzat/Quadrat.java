b811a6447171cedb7c36bdf8095abd62e8908017
Kil <kilianfernandez84@gmail.com>
2026-01-12 08:44:48 +0100

    Completado

---
+++ 15_06_quadrat_reparametritzat/Quadrat.java
@@ -0,0 +1,26 @@
+/*
+ *Kilian Fernández - 9/1/2026
+ * Mostra quadrat de X del tamany del valor donat a args.
+ */
+ 
+public class Quadrat {
+    public static void main(String[] args) {
+    
+    int num = Integer.parseInt(args[0]);
+    String lletra = args[1];
+    
+    if (num < 1) return;
+    
+    dibuixaQuadrat(num, lletra);
+    }   
+       
+    public static void dibuixaQuadrat(int valor, String lletra) {
+        
+        for (int col = 1; col <= valor; col++) {
+            for (int lin = 1; lin <= valor; lin++) {
+            System.out.print(" " + lletra);
+        }
+            System.out.println("");
+        }
+    }    
+}
