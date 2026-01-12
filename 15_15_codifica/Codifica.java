178227fa6556be43c7f4ef2cda5cf647078eabf3
prgtest <kilianfernandez84@gmail.com>
2026-01-12 10:41:21 +0100

    completado ∾ prgtest autocommit 15_15 #1

--- 15_15_codifica/Codifica.java
+++ 15_15_codifica/Codifica.java
@@ -15,20 +15,27 @@ public class Codifica {
     public static void mostraCodificat(String text) {
     
     String textCodificat = "";
+    String abc = "abcdefghijklmnopqrstuvwxy";
     
     for (int i = 0; i < text.length(); i++) {
         char caracter = text.charAt(i);
         
         if (Character.isLetter(caracter)) {
-            //casos caracter = Z || z
-            if (caracter == 'Z') {
-                caracter = 'A';
-                
-            } else if (caracter == 'z') {
+            //cas caracter z 
+            if (caracter == 'z') {
                 caracter = 'a';
                 
             } else {
-                caracter ++;
+            
+                //recorregut abecedari minuscules
+                for (int j = 0; j < abc.length(); j++) {
+                    char charAbc = abc.charAt(j);
+                    
+                    if (charAbc == caracter) {
+                        caracter ++;
+                        break;  
+                    }
+                }                
             }
         }
         textCodificat += caracter;
