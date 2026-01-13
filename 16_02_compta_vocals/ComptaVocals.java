b1abe5df9f8bc78edfa9cdc6a02576b8e9700d9c
Kil <kilianfernandez84@gmail.com>
2026-01-13 09:18:34 +0100

    completado

---
+++ 16_02_compta_vocals/ComptaVocals.java
@@ -0,0 +1,35 @@
+/*
+ *Kilian Fernández - 12/01/2026
+ * Programa que rep un text i mostra totes les vocals catalanes que té
+ */
+ 
+public class ComptaVocals {
+    public static void main(String[] args) {
+        System.out.println("Introdueix un text");
+        String text = Entrada.readLine();
+        
+        String vocals = "aàeèéiíïoòóuúü";
+        
+        //recorregut vocals
+        for (int i = 0; i < vocals.length(); i++) {
+            char vocal = vocals.charAt(i);
+            int comptador = quantesOcurrencies(text, vocal);           
+            mostraOcurrencies(vocal, comptador);
+        }
+        
+    }
+    
+    public static void mostraOcurrencies(char lletra, int quantes) {
+        System.out.println("Nombre de '" + lletra + "'s: " + quantes);
+    }
+    
+    public static int quantesOcurrencies(String text, char lletra) {
+        int comptador = 0;
+        for (int i=0; i < text.length(); i++) {
+            if (text.charAt(i) == lletra) {
+                comptador += 1;
+            }
+        }
+        return comptador;
+    }
+}
