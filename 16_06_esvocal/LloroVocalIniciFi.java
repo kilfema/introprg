ae16a91783f7c97efac7356337f7e61ec7678c9e
prgtest <kilianfernandez84@gmail.com>
2026-01-14 10:32:07 +0100

    completat ∾ prgtest autocommit 16_06 #3

--- 16_06_esvocal/LloroVocalIniciFi.java
+++ 16_06_esvocal/LloroVocalIniciFi.java
@@ -7,41 +7,30 @@ public class LloroVocalIniciFi {
     public static void main(String[] args) {
     
     int comptador = 1;
-    boolean esVocal = false;
+    boolean textValid = false;
        
     while (true) {
 
         System.out.println("Text?");
         String text = Entrada.readLine();
-        
-        if (text.isEmpty()) {
-            confirmarSortida();
-        }
-        
-        String textMinus = text.toLowerCase();
-        
-        for (int i = 0; i < textMinus.length(); i++) {
-            char caracterText = text.charAt(i);
-            esVocal = UtilString.esVocal(caracterText);
-            
-            if (esVocal) break;   
-        }    
+        textValid = analisisText(text);
 
-        if (!esVocal) {
+        boolean confirmarSortida = false;        
+        if (!textValid) {
             confirmarSortida();
+            
+            if (confirmarSortida) return;            
         }
-        
-        
-        boolean confirmarSortida = false;
-        if (confirmarSortida) return;
-        
+
         //lloro
         System.out.println(comptador + ": \"" + text + "\"");
         comptador++;
     }
     }
     
-    public static boolean confirmarSortida() { //modul de confirmació de sortida. quan s'activa, pregunta a l'usuari si vol sortir i crida a UtilitatsConfirmació per valorar si la resposta és afirmativa.
+    public static boolean confirmarSortida() { 
+    /*Modul de confirmació de sortida. quan s'activa, pregunta a l'usuari si vol sortir i crida a    
+    **UtilitatsConfirmació per valorar si la resposta és afirmativa. */
     
             System.out.println("Sortir?");
             String respostaSortir = Entrada.readLine();
@@ -54,4 +43,25 @@ public class LloroVocalIniciFi {
                      
     return false;
     }
+    
+    
+    /* Funcio que rep el text, valida si està vuit, i en cas contrari, crida a esVocal per
+    ** confirmar si té vocals. Retorna booleà */
+    public static boolean analisisText(String text) {
+        boolean esVocal = false;
+         
+        if (text.isEmpty()) {
+            return false;
+        }
+        
+        String textMinus = text.toLowerCase();
+        for (int i = 0; i < textMinus.length(); i++) {
+            char caracterText = textMinus.charAt(i);
+            esVocal = UtilString.esVocal(caracterText);
+ 
+            if (esVocal) return true;   
+        }
+        
+    return false;
+    } 
 }
