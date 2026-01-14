c3f6d2409f18a26de0093d2f76194661f3360bd2
Kil <kilianfernandez84@gmail.com>
2026-01-14 18:31:20 +0100

    completado

---
+++ 16_10_comptavocals/ComptaVocals.java
@@ -0,0 +1,29 @@
+/*
+ *Kilian Fernández - 28/10/2025
+ * Programa que demana un text i el retorna cuantes vocals minuscules té.
+ */
+ 
+public class ComptaVocals {
+    public static void main(String[] args) {
+        
+    System.out.println("Text?");
+    
+    System.out.println(
+        numVocalsMinuscules(
+            Entrada.readLine()
+        )
+    ); 
+    }
+    
+    public static int numVocalsMinuscules(String text) {
+    int comptador = 0;
+       
+    for (int i = 0; i < text.length(); i++) {
+        char caracter = text.charAt(i);
+        
+        if (UtilString.esVocal(caracter)) comptador++;
+    }
+    
+    return comptador;
+    }   
+}
