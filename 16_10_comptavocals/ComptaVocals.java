444cb8b82af2f14eaa82e6e36cc2ce18050d4973
prgtest <kilianfernandez84@gmail.com>
2026-01-19 12:10:23 +0100

    completado ∾ prgtest autocommit 16_10 #2

--- 16_10_comptavocals/ComptaVocals.java
+++ 16_10_comptavocals/ComptaVocals.java
@@ -7,11 +7,10 @@ public class ComptaVocals {
     public static void main(String[] args) {
         
     System.out.println("Text?");
+    String text = Entrada.readLine();
     
     System.out.println(
-        numVocalsMinuscules(
-            Entrada.readLine()
-        )
+        numVocalsMinuscules(text.toLowerCase())
     );   
     }
     
