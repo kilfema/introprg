40c3fa045346ad6fdc6501c004c0b93fb345990b
Kil <kilianfernandez84@gmail.com>
2026-01-20 08:58:06 +0100

    completat

---
+++ 16_15_normalitzador/LloroNormalitzador.java
@@ -0,0 +1,23 @@
+/*
+ *Kilian Fernández - 20/01/2026
+ * Programa que demana un text i el retorna repetint-lo normalitzat; sense caracters especials.
+ */
+ 
+public class LloroNormalitzador {
+    public static void main(String[] args) {
+        
+    while (true) {
+    
+    System.out.println("El lloro espera un text");
+    String text = Entrada.readLine();
+    
+    if (text.isEmpty()) {
+        System.out.println("El lloro s'acomiada atentament");
+        return;
+    }    
+    System.out.println("El lloro respon: " + UtilString.normalitzaText(text));  
+    }                
+    }
+}
+    
+    
