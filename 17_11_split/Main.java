5994fc6f9d7695c96b48d3419cad8cd9933bd203
Kil <kilianfernandez84@gmail.com>
2026-01-23 08:41:04 +0100

    completado

--- 17_11_split/Main.java
+++ 17_11_split/Main.java
@@ -1,19 +1,35 @@
-    /* Aquest programa comprova el funcionament de la funció UtilSTring.separa() */
-    public class Main {
-        public static void main(String[] args) {
-            System.out.println("Text principal?");
-            String principal = Entrada.readLine();
-            System.out.println("Inclou espais?");
-            boolean inclouBlancs = UtilitatsConfirmacio.respostaABoolean(Entrada.readLine());
-            String[] paraules = UtilString.separa(principal, inclouBlancs);
-            System.out.printf("separa(\"%s\", %b) ->%n", principal, inclouBlancs);
-            if (paraules.length > 0) {
-                for (int i=0; i < paraules.length; i++) {
-                    System.out.printf("- \"%s\"\n", paraules[i]);
-                }
-            } else {
-                System.out.println("Cap resultat");
-            }
-        }
+/*
+ *Kilian Fernández - 21/01/2026
+ * 
+ * Aquest programa separa un text en fragments utilitzant els espais en blanc com a separadors, amb
+ * l’opció d’incloure o no aquests espais en el resultat, i retorna un array de String amb el
+ * resultat de la separació.
+ *
+ */
+ 
+public class Main {
+    public static void main(String[] args) {
+    
+    System.out.println("Text principal?");
+    String text = Entrada.readLine();
+    
+    System.out.println("Inclou espais?");
+    boolean inclouBlancs = UtilitatsConfirmacio.respostaABoolean(Entrada.readLine());
+    
+    String[] resultat = UtilString.separa(text, inclouBlancs);
+    
+    if (resultat.length == 0) {
+    
+        System.out.println("Cap resultat");
+        return;
+    }
+    
+    for (int i=0; i<resultat.length; i++) {
+        
+        if (resultat[i] == null) continue;
+        
+        System.out.println("- \"" + resultat [i] + "\"");
     }
 
+    }
+}
