a4795735fa1dffce92e50339c09f49a7d2054fe1
Kil <kilianfernandez84@gmail.com>
2026-02-13 08:55:24 +0100

    completat

---
+++ 17_20_inisequencial/TaulaEnterAString.java
@@ -0,0 +1,157 @@
+/**
+ * Nombre: Kilian Fernández
+ * Fecha: 10/02/2026
+ *
+ * Descripción:
+ * Programa que lee especificaciones de dimensiones de tablas desde los argumentos de la línea
+ * de comandos, valida el formato “n x m” (con n y m entre 1 y 99), crea tablas de enteros con
+ * esas dimensiones, las inicializa con el valor 1 y muestra su contenido formateado.
+ */
+
+public class TaulaEnterAString {
+
+    public static void main(String[] args){
+        
+        int valorInicial = 1;
+        
+        for (int i=0; i<args.length; i++) {
+
+            System.out.println(args[i]);
+
+            int files = obteFiles(args[i]);
+            
+            int columnes = obteColumnes(args[i]);
+            
+            
+            if (files < 1 || columnes < 1) {
+
+                System.out.println("Especificació no vàlida");
+
+            } else {
+                
+                         
+                int[][] taula  = new int[files][columnes];
+
+                UtilTaula.inicialitzaTaula(taula, valorInicial);
+
+                String resultat = UtilTaula.taulaToString(taula);
+
+                System.out.println(resultat);
+
+                System.out.println();
+                
+                valorInicial++;
+
+            }
+
+        }
+
+    }
+
+
+    public static boolean especificacioCorrecta(String especificacio) {
+
+        /* retorna true si l'especificació és de la forma n'x'm on n i m
+
+         * són dos strings d'un o dos dígits.  Exemple: "12x5" seria
+
+         * correcta, mentre que "x3454" no.  */
+
+        String[] coordenades = especificacio.split("x");
+
+        if (coordenades.length != 2) return false;
+
+        for (int i=0; i<coordenades.length; i++) {
+
+            if (! UtilString.esEnter(coordenades[i])) return false;
+
+            int valor = Integer.parseInt(coordenades[i]);
+
+            if (valor < 0 || valor > 99) return false;
+        }
+         
+        return true;
+    }
+
+
+    public static int obteFiles(String especificacio) {
+        
+        String files = "";
+        
+        String col = "";
+        
+        int filesInt = 0;
+        
+        boolean teX = false;
+        
+        for (int i=0; i<especificacio.length(); i++) {
+            
+            char caracter = especificacio.charAt(i);
+            
+            if (!teX) {
+                
+                if (caracter == 'x') {
+                    
+                    teX = true;
+                    
+                    if (i == especificacio.length() - 1) return -1;
+                    
+                    else continue;
+                    
+                } else if (!Character.isDigit(caracter)) return -1;
+                
+                else files += caracter;
+
+            } else {
+            
+                if (!Character.isDigit(caracter)) return -1;   
+                
+                else col += caracter;         
+            }
+        }
+        
+        if (col.isEmpty() || (Integer.parseInt(col) < 1 || Integer.parseInt(col) > 99)) return -1;
+        
+        if (!files.isEmpty()) filesInt = Integer.parseInt(files);
+        
+        if (filesInt>= 1 && filesInt <= 99) return filesInt;
+        
+    return -1;
+    }
+
+
+    public static int obteColumnes(String especificacio) {
+       
+       boolean teX = false;
+       
+       String col = "";
+       
+       String files = "";
+       
+       for (int i=0; i<especificacio.length(); i++) {
+            
+            char caracter = especificacio.charAt(i);
+            
+            if (!teX) {
+                
+                if (caracter == 'x' && i != 0) {
+                    
+                    teX=true;
+                    continue;
+                    
+                } else if (!Character.isDigit(caracter)) return -1;
+                
+                 else files += caracter;
+                
+            } else if (Character.isDigit(caracter)) col+= caracter;
+       }
+       
+       if (col.isEmpty() || col.length() > 2) return -1;
+       
+        else if (files.isEmpty() || 
+            (Integer.parseInt(files) < 1 || Integer.parseInt(files) > 99)) return -1;
+       
+       return Integer.parseInt(col);
+    }
+
+}
---
+++ 17_20_inisequencial/UtilTaula.java
@@ -0,0 +1,64 @@
+/*
+*
+*  Kilian Fernández - 09/02/2026
+*
+* Biblioteca de utilidades para operar sobre tablas de caracteres,
+* proporcionando funciones puras de transformación sin modificar
+* las estructuras originales.
+*
+*/
+public class UtilTaula {
+    
+    /*Recorre una tabla rectangular de caracteres y construye una nueva tabla sustituyendo un carácter
+    ** origen por uno destino, sin modificar la estructura ni el contenido de la tabla original.
+    */
+    public static char[][] substitueix(char taula[][], char primer, char segon) {
+        
+        char[][] desti = new char[taula.length][taula[0].length];
+        
+        for (int i=0; i<desti.length; i++) {
+            
+            for (int j=0; j<desti[0].length; j++) {
+                 
+                if (taula[i][j] == primer) desti[i][j] = segon;
+                
+                else desti[i][j] = taula[i][j];
+            }
+        }
+        
+        return desti;
+    }
+    
+    /* Funció pura que composa un String a partir del contingut d'una taula d'enters. Els valors
+    ** apareixeran separats amb un espai i representa els números com a mínim amb vuit posicions. 
+    */
+    public static void inicialitzaTaula(int[][] taula, int valorInicial) {
+        
+        
+        for (int i=0; i<taula.length; i++) {
+            for (int j=0;j<taula[0].length; j++) {
+                
+                taula[i][j] = valorInicial;
+                
+                valorInicial++;
+            }
+        }
+    }
+    
+    public static String taulaToString(int[][] taula) {
+        
+        String resultat = "";
+
+        
+        for (int i=0; i<taula.length; i++) {
+            
+            for (int j=0; j<taula[0].length; j++) {
+                                
+                resultat += String.format("%8d", taula[i][j]);                               
+            }
+            resultat += "\n";
+        }
+        
+        return resultat;  
+    }
+}
